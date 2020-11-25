eee4ce3da1aa0441c000f78b80e415d64abd089e
BY <byagihas@gmail.com>
2020-11-25 14:49:12 -0600

    cleaning syntax, update latest

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const getPosts = require('./getWallpaperPosts.js');
 const downloadWallpaper = require('./downloadWallpapers.js');
 
 const main = () => {
-    const driver = async () => {
+    const driver = async (wallpapercount) => {
         // get 10 images from r/wallpapers
-        let p = await getPosts('wallpapers', 20);
+        let p = await getPosts('wallpapers', wallpapercount);
         let d = await downloadWallpaper(p);
         if(p) {
             return d;
         }
     };
-    driver();
+    driver(20);
 };
 
 main();
